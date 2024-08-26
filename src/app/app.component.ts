51ec7b3aa001d1fe6cb0abcc8bde62a1ae99beba
wmancera <wmancera@bancobacc.com.do>
2024-08-26 17:32:52 -0400

    Initial template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'visionUI';
+
+  constructor(private themeService: ThemeService) {}
+
+  changeTheme(theme: string) {
+    this.themeService.switchTheme(theme);
+  }
 }
