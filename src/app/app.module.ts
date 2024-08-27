2149887f77d07fe13c42eb030f8a04d435369a03
wmancera <wmancera@bancobacc.com.do>
2024-08-27 15:27:48 -0400

    Complete theming configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { ThemesComponent } from './layout/themes/themes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LayoutComponent,
-    ThemesComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, LayoutComponent, ThemesComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '../core/services/theme.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.scss'
+  styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  constructor(private themeService: ThemeService) {}
 
+  ngOnInit(): void {
+    // const savedTheme = localStorage.getItem('selected-theme');
+    // if (savedTheme) {
+    //   this.themeService.switchTheme(savedTheme);
+    // }
+  }
+  changeTheme(theme: string) {
+    this.themeService.switchTheme(theme);
+    localStorage.setItem('selected-theme', theme);
+  }
 }
